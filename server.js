481d92b8f15bee8d64b7c1257037683d43e6fac9
NikoVl <pedinovl97@gmail.com>
2025-08-03 15:12:27 +0200

    Struktrur

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 
